3afd0af6979d188efee815ef69f6c7cf5029e767
Rohan Wadadar <135444253+rohanwadadar@users.noreply.github.com>
2024-09-08 21:56:50 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -14,11 +14,10 @@ function App() {
       <Navbar />
       <Hero />
       <Statistics />
-      <Product />
-      
-      <ProductListing />
+  
+     <Product />
       <Routes>
-        <Route path="/" element={<Product />} />
+        <Route path="/product" element={<Product />} />
         <Route path="/productList" element={<ProductListing />} />
         <Route path="/productDetails/:id" element={<ProductDetails />} /> {/* Example route for product details */}
       </Routes>
